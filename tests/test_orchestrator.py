67eedbb54a414ef1eda8ab6257510dcf7b546158
Laura Venancio Martins <laura.venancio@dinfor.uniceub>
2025-11-25 09:43:00 -0300

    Teste orchestrator

---
+++ tests/test_orchestrator.py
@@ -0,0 +1,7 @@
+from orchestrator.orchestrator import Orchestrator
+
+
+def test_orchestrator_creates_workers():
+    servers = [{'id':1,'capacidade':1},{'id':2,'capacidade':1}]
+    orch = Orchestrator(servers)
+    assert len(orch.workers) == 2
